c90941c4eeb2fcac905f553ba0c9c99c04ef70c5
djumanov <diyorbekjumanov1120@gmail.com>
2023-09-14 09:34:09 +0500

    tasks

--- db.py
+++ db.py
@@ -7,30 +7,43 @@ db = TinyDB('db.json', indent=4)
 users = db.table('users')
 
 
-def get_users(n: int) -> list[dict]:
-    url = "https://randomuser.me/api/"
-    payload = {"results": n}
-
-    response = requests.get(url=url, params=payload)
-
-    if response.status_code == 200:
-        users = []
-        for user in response.json()['results']:
-            users.append({
-                'first_name': user['name']['first'],
-                'last_name': user['name']['last'],
-                'gender': user['gender'],
-                'country': user['location']['country'],
-                'city': user['location']['city'],
-                'email': user['email'],
-                'age': user['dob']['age'],
-                'phone': user['phone'],
-                'nat': user['nat'],
-            })
-
-        return users
+def user_by_id(id: int) -> Document:
+    pass
 
 
-users_data = get_users(5000)
-users.insert_multiple(users_data)
+def users_by_id(id: int) -> list[Document]:
+    pass
+
+
+def users_by_gender(gender: str) -> list[Document]:
+    pass
+
+
+def users_by_country(country: str) -> list[Document]:
+    pass
+
+
+def users_by_city(city: str) -> list[Document]:
+    pass
+
+
+def users_by_age(age: int) -> list[Document]:
+    pass
+
+
+def users_by_nat(nat: str) -> list[Document]:
+    pass
+
+
+def users_in_range(min_age: int, max_age: int) -> list[Document]:
+    pass
+
+
+def users_by_country_and_city(country: str, city: str) -> list[Document]:
+    pass
+
+
+def users_by_country_and_city(country: str, city: str) -> list[Document]:
+    pass
+
 
