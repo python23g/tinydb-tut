2644387fb64a8370572006f0de24f22999310566
djumanov <diyorbekjumanov1120@gmail.com>
2023-09-14 09:16:52 +0500

    randomusers

---
+++ db.py
@@ -0,0 +1,40 @@
+from tinydb import TinyDB
+from tinydb.database import Document
+import requests
+
+db = TinyDB('db.json', indent=4)
+
+males = db.table('males')
+females = db.table('females')
+
+
+def get_users(n: int, gender: str) -> list[dict]:
+    url = "https://randomuser.me/api/"
+    payload = {"results": n, "gender": gender}
+
+    response = requests.get(url=url, params=payload)
+
+    if response.status_code == 200:
+        users = []
+        for user in response.json()['results']:
+            users.append({
+                "full_name": f'{user["name"]["first"]} {user["name"]["last"]}',
+                "age": user['dob']['age'],
+                'address': f'{user["location"]["country"]}, {user["location"]["city"]}, {user["location"]["street"]["number"]}',
+                'phone': user['phone'],
+                'email': user['email'],
+                'image': user['picture']['large']
+            })
+
+        return users
+
+
+def add_users(users: list[dict], table: TinyDB):
+    table.insert_multiple(users)
+
+
+male_users = get_users(30, 'male')
+female_users = get_users(30, 'female')
+
+add_users(male_users, males)
+add_users(female_users, females)
